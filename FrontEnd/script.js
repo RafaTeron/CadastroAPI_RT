e8b906927ee0c8f4a4388d3eff17dba938f6bb8c
RafaTeron <rafael_sabreu@hotmail.com>
2023-03-26 15:32:25 -0300

    FrontEnd

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,51 @@
 const formulario = document.querySelector("form");
+const Ilogin = document.querySelector(".login")
+const Inome = document.querySelector(".nome")
+const Isenha = document.querySelector(".senha")
+const Iemail = document.querySelector(".email")
+const Igenero = document.querySelector(".genero")
+const Icpf = document.querySelector(".cpf")
+const Icep = document.querySelector(".cep")
+const Icelular = document.querySelector(".celular")
 
-console.log(formulario);
+function cadastrar(){
+
+    fetch("http://localhost:8080/cadastrar",
+        {
+            headers: {
+                'Accept': 'applcation/json',
+                'Content-Type': 'application/json'
+            },
+            method:"POST",
+            body: JSON.stringify({
+                login: Ilogin.value,
+                nome: Inome.value,
+                senha: Isenha.value,
+                email: Iemail.value,
+                genero: Igenero.value,
+                cpf: Icpf.value,
+                cep: Icep.value,
+                celular: Icelular.value
+            })
+        })
+        .then(function(res) {console.log(res)})
+        .catch(function(res) {console.log(res)})    
+};
+
+function limpar() {
+    Ilogin.value = "";
+    Inome.value = "";
+    Isenha.value = "";
+    Iemail.value = "";
+    Igenero.value = "";
+    Icpf.value = "";
+    Icep.value = "";
+    Icelular.value = "";
+}
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+    
+    cadastrar();
+    limpar();
+});
